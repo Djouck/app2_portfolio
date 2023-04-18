faa948dbc1ee06c3774d4573fc45988437a0f3e6
giulia <gilia.ruffini445@gmail.com>
2023-04-18 17:16:06 +0200

    Day 23: Set environment variable and Update link for To-Do App and for Portfolio App

--- send_email.py
+++ send_email.py
@@ -1,11 +1,14 @@
 import smtplib, ssl
+import os
 
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
+
     username = "giulia.ruffini445@gmail.com"
-    password = "khppbeksdhzciiap"
+    password = os.getenv("PASSWORD")
+
     receiver = "giulia.ruffini@unito.it"
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(host, port, context=context) as server:
