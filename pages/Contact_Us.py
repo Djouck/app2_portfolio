7e4ec06a02c8b50872f4b5c9f50951009634acb5
giulia <gilia.ruffini445@gmail.com>
2023-04-18 22:22:57 +0200

    Day 23: Fix button bug_1

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -3,7 +3,7 @@ from send_email import send_email
 
 st.header("Contact Me")
 
-with st.form(key="my_form"):
+with st.form(key="my_form", clear_on_submit=True):
     user_email = st.text_input("Your email address")
     raw_message = st.text_area("Your message")
     message = f"""\
@@ -12,8 +12,8 @@ Subject: New email from {user_email}
 From: {user_email}
 {raw_message}
 """
-    button = st.form_submit_button(clear_on_submit=True)
+    button = st.form_submit_button()
     if button:
         send_email(message)
-
         st.info("Your email was sent successfully!")
+
